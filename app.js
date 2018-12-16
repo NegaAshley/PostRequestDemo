dac19e32d6cdb1867db26cd42c7d8d9824c803ac
NegaAshley <ashleymxu@gmail.com>
2018-12-16 01:43:34 +0000

    Adding header and footer templates and adding post route

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ var app = express();
 //Default view engine
 app.set("view engine", "ejs");
 
+//Tells Express to serve contents of public directory
+app.use(express.static("public"));
+
 //Root route
 app.get("/", function(req, res){
     res.render("home");    
 });
 
+app.post("/addFriend", function(req, res){
+    res.send("YOU HAVE REACHED THE POST ROUTE!");
+});
+
 app.get("/friends", function(req, res){
-    res.render("friends");
+    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends", {friends: friends});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
