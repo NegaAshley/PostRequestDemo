9aad1e699e9919a145dc0952be73cfeffcfd5bf1
NegaAshley <ashleymxu@gmail.com>
2018-12-16 01:25:27 +0000

    Adding friends route

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 
+//Default view engine
 app.set("view engine", "ejs");
 
+//Root route
 app.get("/", function(req, res){
     res.render("home");    
+});
+
+app.get("/friends", function(req, res){
+    res.render("friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
